3785ac967f6f00f9504c144d7029f5440e9fce27
bruno.rodrigues <bruno.rodrigues@vertown.com>
2022-05-07 18:22:49 -0300

    screens implementation

--- src/app/jogo-da-velha/jogo-da-velha.component.ts
+++ src/app/jogo-da-velha/jogo-da-velha.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { JogoDaVelhaService } from './shared';
 
 @Component({
   selector: 'app-jogo-da-velha',
   templateUrl: './jogo-da-velha.component.html',
-  styleUrls: ['./jogo-da-velha.component.css']
+  styleUrls: ['./jogo-da-velha.component.css'],
 })
 export class JogoDaVelhaComponent implements OnInit {
-
-  constructor() { }
+  constructor(private jogoDaVelhaService: JogoDaVelhaService) {}
 
   ngOnInit(): void {
+    this.jogoDaVelhaService.inicializar();
   }
 
+  get showInicio(): boolean {
+    return this.jogoDaVelhaService.showInicio;
+  }
+
+  get showTabuleiro(): boolean {
+    return this.jogoDaVelhaService.showTabuleiro;
+  }
+
+  get showFinal(): boolean {
+    return this.jogoDaVelhaService.showFinal;
+  }
+
+  iniciarJogo(): void {
+    this.jogoDaVelhaService.iniciarJogo();
+  }
 }
